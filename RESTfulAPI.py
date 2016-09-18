79b932044c5fd2a1b2a77e886d7665a2ee819e6d
gancj <gancj@ucweb.com>
2016-09-18 16:56:53 +0800

    RESTful

---
+++ RESTfulAPI.py
@@ -0,0 +1,42 @@
+# -*- coding:UTF-8 -*-
+
+__author__ = 'gancj'
+
+__data__ = '2016-09-18 11:48'
+
+__mail__ = '393037282@qq.com'
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    task = filter(lambda t: t['id'] == task_id, tasks)
+    if len(task) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'title' in request.json and type(request.json['title']) != unicode:
+        abort(400)
+    if 'description' in request.json and type(request.json['description']) is not unicode:
+        abort(400)
+    if 'done' in request.json and type(request.json['done']) is not bool:
+        abort(400)
+    task[0]['title'] = request.json.get('title', task[0]['title'])
+    task[0]['description'] = request.json.get('description', task[0]['description'])
+    task[0]['done'] = request.json.get('done', task[0]['done'])
+    return jsonify({'task': task[0]})
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    task = filter(lambda t: t['id'] == task_id, tasks)
+    if len(task) == 0:
+        abort(404)
+    tasks.remove(task[0])
+    return jsonify({'result': True})
+
+
+if __name__ == '__main__':
+
+    app.debug = True
+    app.run()
